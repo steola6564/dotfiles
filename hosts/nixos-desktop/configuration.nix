c4c8bfdd4dea9630cedd1af547ddde4d2af1f9e8
steola <ishishi.steola@outlook.com>
2025-09-10 00:42:08 +0900

    organise nixos-desktop/configuration.nix

--- hosts/nixos-desktop/configuration.nix
+++ hosts/nixos-desktop/configuration.nix
@@ -5,231 +5,45 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ 
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    
+    # System
+    ../../modules/system/base.nix
+    ../../modules/system/boot.nix
 
-  # allowUnfree
-  nixpkgs.config.allowUnfree = true;
+    # Networking
+    ../../modules/networking/common.nix
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    # UI & Desktop
+    ../../modules/ui/fonts.nix
+    ../../modules/ui/desktop.nix
 
-  fonts = {
-    packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      noto-fonts-emoji
-      # 好みで（どちらかを使うなら下3行は任意）
-      source-han-sans-japanese     # 源ノ角ゴシック
-      # source-han-serif-japanese    # 源ノ明朝
-      nerd-fonts.jetbrains-mono
-      nerd-fonts.fira-code
-    ];
-    fontconfig = {
-      enable = true;
-      allowBitmaps = false;     # ビットマップを使わない
-      antialias = true;
-      hinting.enable = true;
-      hinting.style = "slight"; # 文字のにじみ少なめ
-      subpixel.rgba = "rgb";
-      defaultFonts = {
-        serif = [ "Noto Serif CJK JP" "Noto Serif" ];
-        sansSerif = [ "Noto Sans CJK JP" "Noto Sans" ];
-        monospace = [ "Noto Sans Mono CJK JP" "Noto Sans Mono" ];
-        emoji = [ "Noto Color Emoji" ];
-      };
-    };
-  };
+    # Input/Locale
+    ../../modules/i18n/input.nix
 
+    # Hardware
+    ../../modules/hardware/nvidia.nix
 
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_6_6;
+    # Audio
+    ../../modules/audio/pipewire.nix
 
-  networking.hostName = "nixos-desktop"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+    # Users
+    ../../modules/users/steola.nix
 
-  # Set your time zone.
-  time.timeZone = "Asia/Tokyo";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.inputMethod = {
-    type = "fcitx5";
-    enable = true;
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-      libsForQt5.fcitx5-qt
-      qt6Packages.fcitx5-qt
-    ];
-  };
-
-  console = {
-    font = "Lat2-Terminus16";
-    # keyMap = "us";
-    useXkbConfig = true; # use xkb.options in tty.
-  };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  services.xserver = {
-    enable = true;
-    xkb.layout = "us";
-    xkb.options = "terminate:ctrl_alt_bksp";
-
-    videoDrivers = [ "nvidia" ];
-    displayManager.gdm.enable = true;
-    displayManager.gdm.wayland = true;
-    desktopManager.gnome.enable = true;
-  };
- 
-  programs.hyprland.enable = true;
-
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-
-  # services.displayManager.defaultSession = "sway";
-  
-  # programs.sway = {
-  #   enable = true;
-  #   wrapperFeatures.gtk = true;
-  # };
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = true;
-    powerManagement.enable = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
-  };
-
-  environment.sessionVariables = {
-    "__GLX_VENDOR_LIBRARY_NAME" = "nvidia";
-    "GBM_BACKEND" = "nvidia-drm";
-    "WLR_NO_HARDWARE_CURSORS" = "1";
-    # "WLR_RENDERER" = "vulkan";
-    # GTK_IM_MODULE = "fcitx";
-    QT_IM_MODULE = "fcitx";
-    XMODIFIERS = "@im=fcitx";
-  };
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.steola = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-      bat
-    ];
-    shell = pkgs.zsh;
-  };
-
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-
-  programs.firefox.enable = true;
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    neovim
-    git
-    curl
-    grim
-    slurp
-    wl-clipboard
-    mako
-    firefox
-    brave
-    alacritty
-    waybar
-    brightnessctl
-    pavucontrol
-    hyprland
-    hyprpaper
-    nautilus
-    rofi-wayland
-    xdg-desktop-portal-hyprland
-    home-manager
+    # Packages & Services
+    ../../modules/pkgs/common.nix
+    ../../modules/services/common.nix
+    
+    # Home Manager integration
+    # If you prefer, you can also keep HM in the flake's 'homeConfigurations'.
+    # Otherwise import this module to configure HM alongside the system.
+    # ../../modules/home-manager.nix
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  networking.hostName = "nixos-desktop";
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
 
   home-manager = {
--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -4,7 +4,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_6_6;
   };
 }
 
