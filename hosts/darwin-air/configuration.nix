6c75eaad673d19ccef581c8d19db494de518bb14
Taiga Ishii <steola@darwin-air.local>
2025-09-16 17:03:30 +0900

    merge mac's nix files

---
+++ hosts/darwin-air/configuration.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  # 必須：新規インストールの基準値（今は 6）
+  system.stateVersion = 6;
+
+  # nix を有効化（有効なら nix-daemon も自動管理される）
+  nix.enable = true;
+
+  # ルート側も flakes/nix-command を宣言的にON（おすすめ）
+  # nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # programs.zsh.enable = true;
+  # nixpkgs.config.allowUnfree = true;
+
+  # services.nix-daemon.enable = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  programs.zsh.enable = true;
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+    # curl
+    # vim
+  ];
+
+  home-manager.users.steola = { ... }: {
+    imports = [ ../../modules/home/darwin/home.nix ];
+  };
+}
+
---
+++ modules/home/darwin/home.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "steola";
+  home.homeDirectory = "/Users/steola";
+  home.stateVersion = "25.05"; # nixpkgsと合わせる
+
+  programs.git.enable = true;
+  programs.zsh.enable = true;
+
+  home.packages = with pkgs; [
+    # htop
+    # jq
+  ];
+}
+
