d135a88f75e509bddb20593ffeda7fe08c389234
steola <ishishi.steola@outlook.com>
2025-09-18 19:38:31 +0900

    organise hosts/darwin-air/configuration.nix

--- hosts/darwin-air/configuration.nix
+++ hosts/darwin-air/configuration.nix
@@ -1,33 +1,19 @@
 { config, pkgs, ... }:
 {
-  imports = [ 
-    ../../modules/home-manager.nix
-  ];
-
   # 必須：新規インストールの基準値（今は 6）
   system.stateVersion = 6;
 
   # nix を有効化（有効なら nix-daemon も自動管理される）
   nix.enable = true;
-
-  # ルート側も flakes/nix-command を宣言的にON（おすすめ）
-  # nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # programs.zsh.enable = true;
-  # nixpkgs.config.allowUnfree = true;
-
-  # services.nix-daemon.enable = true;
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  programs.zsh.enable = true;
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    git
-    # curl
-    # vim
-  ];
+  # 最近の変更でnix.enable = true;のみで良くなった
+  # services.nix-daemon.enable = true;
+
   users.users.steola.home = "/Users/steola";
+  imports = [ 
+    ../../modules/home-manager.nix
+  ];
 }
 
