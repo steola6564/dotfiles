5fca38992e8bf1b34823cb3706deac377a7e3a14
steola <ishishi.steola@outlook.com>
2025-09-25 00:43:40 +0900

    Re*remove nixpkgs alacritty

--- modules/home/darwin/base.nix
+++ modules/home/darwin/base.nix
@@ -7,7 +7,6 @@
     # jq
     # htop
   ];
-  programs.alacritty.enable = true;
 
   home.activation.setTrackpadSpeed = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     /usr/bin/defaults write -g com.apple.trackpad.scaling -float 7.5
