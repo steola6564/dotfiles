f5118bf827cfedff3bff3116faaeb3d7d5029fad
steola <ishishi.steola@outlook.com>
2025-10-05 03:23:33 +0900

    add agekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,9 +3,11 @@
   # 例: cloudflared のトークンを暗号化するファイルを定義
   "cloudflared-credentials.age".publicKeys = [
     "age13sjyfypwvm375ywwvntkcyydwz5avrn6tkl9075dducwn6tym96qmhuf5p"
+    "age1r36ddxgempjfyz6lly3zmnhut0svvt894phu55y72k4jfpqq4yzqpnkrnn"
   ];
   "tf-cloudflare.json.age".publicKeys = [
     "age13sjyfypwvm375ywwvntkcyydwz5avrn6tkl9075dducwn6tym96qmhuf5p"
+    "age1r36ddxgempjfyz6lly3zmnhut0svvt894phu55y72k4jfpqq4yzqpnkrnn"
   ];
 }
 
