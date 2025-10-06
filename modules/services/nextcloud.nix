18dd2319c5b76e92af22f8887c4b2198c9634756
steola <taiga.ishishi@gmail.com>
2025-10-06 15:26:40 +0900

    setting is now work

--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -6,21 +6,60 @@
     package = pkgs.nextcloud30;
     hostName = "cloud.niboratory.com";
     https = true;                   # Cloudflare側でTLS終端
-    nginx.enableACME = false;       # Let's Encrypt不要（CloudflareでProxy化）
     maxUploadSize = "10G";
+
     config = {
       adminuser = "admin";
       adminpassFile = "/var/lib/nextcloud/admin-pass";
+      dbtype = "sqlite";
+      appstoreEnable = true;
+      appsPaths = [ "/var/lib/nextcloud/apps" ];
     };
+
     datadir = "/var/lib/nextcloud/data";
   };
 
   services.nginx.enable = true;
 
+
+  # services.mysql = {
+    # enable = true;
+    # package = pkgs.mariadb;
+    # ensureDatabases = [ "nextcloud" ];
+    # ensureUsers = [
+      # {
+        # name = "nextcloud";
+        # ensurePermissions = {
+          # "nextcloud.*" = "ALL PRIVILEGES";
+        # };
+      # }
+    # ];
+  # };
+
+  # services.nextcloud = {
+    # enable = true;
+    # package = pkgs.nextcloud30;
+    # hostName = "cloud.niboratory.com";
+    # https = true;
+    # maxUploadSize = "10G";
+    # config = {
+      # adminuser = "admin";
+      # adminpassFile = "/var/lib/nextcloud/admin-pass";
+      # dbtype = "mysql";
+      # dbuser = "nextcloud";
+      # dbhost = "localhost";
+      # dbname = "nextcloud";
+    # };
+    # datadir = "/var/lib/nextcloud/data";
+  # };
+
+
   # Cloudflareトンネル経由アクセス向けヘッダ設定を安全にする
   services.nginx.virtualHosts."cloud.niboratory.com" = {
-    forceSSL = true;
-    enableACME = false;
+    listen = [
+      { addr = "127.0.0.1"; port = 8080; ssl = false; }
+    ];
+    forceSSL = false;
     extraConfig = ''
       set_real_ip_from 103.21.244.0/22;
       set_real_ip_from 103.22.200.0/22;
