1c1a567484f8669275f5dfa3a42de23ce56844a0
steola <ishishi.steola@outlook.com>
2025-10-09 11:11:25 +0900

    display settings

--- modules/darwin/display.nix
+++ modules/darwin/display.nix
@@ -6,7 +6,7 @@
   };
 
   system.activationScripts.setDisplay.text = ''
-    /opt/homebrew/bin/displayplacer "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1680x1050 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0"
+    /opt/homebrew/bin/displayplacer "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1710x1112 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0"
   '';
 }
 
