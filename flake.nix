807543ffd4474741356367e7fecde6153175ceff
steola <ishishi.steola@outlook.com>
2026-01-11 00:10:08 +0900

    delete nixos-server settings in flake.nix

--- flake.nix
+++ flake.nix
@@ -66,32 +66,6 @@
         default = import ./overlays/cloudflared.nix;
     };
 
-    nixosConfigurations.nixos-server = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {
-        inherit inputs;
-	hostname = "nixos-server";
-      };  # modules/ops/agenix.nix で使う
-      modules = [
-        ./hosts/nixos-server/configuration.nix
-        home-manager.nixosModules.home-manager
-
-        # overlay を有効化 + unfree許可維持
-        ({ pkgs, ... }: {
-          nixpkgs = {
-            overlays = [ self.overlays.default ];
-            config = {
-              allowUnfree = true;
-              # 元の意図：terraform と cloudflared-bin を許可
-              allowUnfreePredicate = pkg:
-                builtins.elem (pkgs.lib.getName pkg) [ "terraform" "cloudflared-bin" ];
-            };
-          };
-        })
-      ];
-    };
-
-
     nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
